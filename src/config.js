2952023ee74765d6f04b3a82a3c8bff6459be26c
ximpelz <ximpelz@foxmial.com>
2026-04-27 19:30:28 +0800

    change cfg

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   // DDNSPOD的secretId和secretKey 获取地址: https://console.dnspod.cn/account/token/apikey
-  secretId: "",
-  secretKey: "",
+  secretId: env.DNSPOD_SEC_ID,
+  secretKey: env.DNSPOD_SEC_KEY,
   // 主域名和需要DDNS的子域名
-  Domain: "vvhan.com",
-  SubDomain: "cf.cname"
+  Domain: env.Domain,
+  SubDomain: env.SubDomain
 };
