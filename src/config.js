7cf427b8c9bb2704808065f67f9bfa718e8b2289
ximpelz <ximpelz@foxmial.com>
2026-04-28 11:23:28 +0800

    fix env

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   // DDNSPOD的secretId和secretKey 获取地址: https://console.dnspod.cn/account/token/apikey
-  secretId: env.DNSPOD_SEC_ID,
-  secretKey: env.DNSPOD_SEC_KEY,
+  secretId: process.env.DNSPOD_SEC_ID,
+  secretKey: process.env.DNSPOD_SEC_KEY,
   // 主域名和需要DDNS的子域名
-  Domain: env.Domain,
-  SubDomain: env.SubDomain
+  Domain: process.env.Domain,
+  SubDomain: process.env.SubDomain
 };
